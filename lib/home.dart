675c93875abd49c510ba56662f2b6a1d5bc51437
m.steyer <m.steyer@finatix.de>
2022-02-23 16:37:37 +0100

    some experimentation with layout

---
+++ lib/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Personal Expenses App"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: Container(color: Colors.green),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(color: Colors.blue),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            child: Text("A"),
+                            color: Colors.red[300],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Text("B"),
+                      color: Colors.yellow[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
